099c86a0d8e406951e51651d46c0f6737f30a7f5
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-09-30 23:12:23 +0900

    add:shadcnのインストール

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
 }
