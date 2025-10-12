c825137def84f210d6caa3e198f5e172ed72f99f
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-10-12 19:11:20 +0900

    add:各種APIの作成

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,13 +9,45 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>();
 
 // Accessing D1 is via the c.env.YOUR_BINDING property
-app.get("/query/users/:id", async (c) => {
-  const id = c.req.param("id");
+// app.get("/", (c) => {
+//   return c.json({ message: "API is running!" });
+// });
+
+app.get("/", (c) => {
+  return c.text("Hello from Hono!");
+});
+
+app.get("/api/todos", async (c) => {
   try {
-    let { results } = await c.env.hono_todo_app.prepare("SELECT * FROM todos WHERE id = ?").bind(id).run();
-    return c.json(results);
-  } catch (e) {
-    return c.json({ err: e.message }, 500);
+    const db = c.env.hono_todo_app;
+    const results = await db.prepare("SELECT * FROM Todos").all();
+    return c.json({ todos: results.results });
+  } catch (error) {
+    return c.json({ error: "Failed to fetch todos" }, 500);
+  }
+});
+
+app.post("/api/posts", async (c) => {
+  try {
+    const db = c.env.hono_todo_app;
+    const results = await db.prepare("INSERT INTO Todos (title,completed) VALUES (?,?) RETURNING *").bind("test", false).run();
+
+    return c.json({ postId: results.meta.last_row_id, todo: results.results });
+  } catch (error) {
+    return c.json({ error: "Failed to create post" }, 500);
+  }
+});
+
+app.delete("/api/delete", async (c) => {
+  try {
+    const db = c.env.hono_todo_app;
+    const { id } = await c.req.json();
+    console.log("Deleting todo with id:", id);
+    const results = await db.prepare("DELETE FROM Todos WHERE id = ? RETURNING *").bind("27").run();
+
+    return c.json({ deletedId: id, todo: results.results });
+  } catch (error) {
+    return c.json({ error: "Failed to delete todo" }, 500);
   }
 });
 
